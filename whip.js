b11881fd170fa0b5425a0e7af79c171cc5fe83c9
Joe Mama <112326841+NoDoorsNoWhores@users.noreply.github.com>
2022-08-30 03:41:04 +0300

    New Whip Arrived

---
+++ whip.js
@@ -0,0 +1,27 @@
+whip = {
+  NumberOfWheels = 3,
+  MaxSpeed = 69,
+  HorsePower = 15,
+  Fuel = 'Liquid Orchid Fertilizer',
+  HasDoors = false,
+  BitchesOnBoard = 6,
+  GloveBox = {
+    Money = 69420.69,
+    Condoms = 17,
+    Weapons = [
+      Machete, Glock-18, OldFirework
+    ],
+  },
+  SteeringWheel = {
+    Material = 'African Blackwood',
+    IsDismantleble = true,
+    HasHorn = true
+  },
+  BrakingDistance = 20,
+  Color = 'blue',
+  HasAutopilot = true,
+  Pedals = 3,
+  HasRoof = false,
+  CarTheftAttempts = 9,
+  HasAlarm = true,
+}
